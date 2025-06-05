efa9a60273317075347a4653abfe1a3a11e6956b
Ajay <mailtoajay7007@gmail.com>
2025-06-05 17:36:55 +0530

    text and sky movement

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { use, useState } from "react";
 import { FaGithub } from "react-icons/fa";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
@@ -29,6 +29,25 @@ function App() {
       },
     });
   });
+
+  useGSAP(() => {
+    const main = document.querySelector(".main");
+
+    main?.addEventListener("mousemove", (e) => {
+      const xMove = (e.clientX / window.innerWidth - 0.5) * 40;
+
+      gsap.to(".landing .text", {
+        x: `${xMove * 0.05}%`,
+        ease: "power1.out",
+        duration: 0.5,
+      });
+      gsap.to(".imagesdiv .sky", {
+        x: `${xMove * 0.07}%`,
+        ease: "power1.out",
+        duration: 0.5,
+      });
+    });
+  }, [showContent]);
 
   return (
     <>
@@ -78,28 +97,37 @@ function App() {
                 </a>
               </div>
             </nav>
+
+            <div className="textdiv absolute top-0 left-0 text-[8rem] sm:text-[9.5rem] font-extrabold z-[80] scroll-snap-start flex flex-col items-center justify-center w-full h-screen leading-none opacity-80 text-white">
+              <div className="text flex flex-col items-center justify-center">
+                <h1 className="sm:pr-40">grand</h1>
+                <h1 className="sm:pl-20">theft</h1>
+                <h1 className="sm:pr-40">auto</h1>
+              </div>
+            </div>
+
             <div className="imagesdiv w-full h-screen relative overflow-hidden ">
               <img
                 src="./sky.png"
                 alt="Sky"
-                className="w-full h-full object-cover absolute"
+                className=" sky w-full h-full object-cover absolute scale-125"
               />
               <img
                 src="./bg.png"
                 alt="Background"
-                className="w-full h-full object-cover absolute"
+                className=" bg w-full h-full object-cover absolute"
               />
               <img
                 src="./girlbg.png"
                 alt="Background"
-                className="absolute sm:-bottom-[85%] left-1/2 -translate-x-1/2 z-[110] bottom-10 sm:scale-100 scale-125 "
+                className=" girl absolute  shadow-black drop-shadow-2xl sm:-bottom-[85%] left-1/2 -translate-x-1/2 z-[110] bottom-10 sm:scale-100 scale-125 "
               />
             </div>
 
             <div className="btmbar w-full h-[10vh] z-[200]  bg-gradient-to-t from-black to-transparent fixed bottom-0 left-0  py-14 px-10 ">
               <div className="flex items-center  gap-4 text-white ">
-                <i className="ri-arrow-down-line text-3xl"></i>
-                <h3 className="font-[arial] text-xl ">
+                <i className="ri-arrow-down-line sm:text-3xl"></i>
+                <h3 className="font-[arial] hidden sm:block  text-xl ">
                   Scroll Down
                 </h3>
               </div>
@@ -109,7 +137,6 @@ function App() {
                 alt="PS5"
                 className="absolute top-1/2 left-1/2 -translate-x-1/2  h-[60px] "
               />
-              
             </div>
           </div>
         </div>
